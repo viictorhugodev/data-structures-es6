5b8e9f7268927debfa4997ed0444789c16ad8579
Victor Hugo Segundo Aguilar <victorhugo.webdeveloper@outlook.com>
2025-03-04 11:53:43 -0600

    [UPDATE] - adding new array

--- linear-table/main.js
+++ linear-table/main.js
@@ -1,5 +1,19 @@
-let scores = new Array(90,70,50,80,60,85);
+// let scores = new Array(90,70,50,80,60,85);
 
-for(let i = 0; i < scores.length; i++) {
-  document.write(scores[i] + ",");
-}
+// for(let i = 0; i < scores.length; i++) {
+//   document.write(scores[i] + ",");
+// }
+
+// let scoresTemp = new Array(90,70,50,80,60,85, 75);
+
+// for(let i = 0; i < scoresTemp.length; i++) {
+//   document.write(scoresTemp[i] + ",");
+// }
+
+// Primer arreglo
+let scores = [90, 70, 50, 80, 60, 85];
+document.getElementById("scores-container").innerHTML = scores.join(", ");
+
+// Segundo arreglo
+let scoresTemp = [90, 70, 50, 80, 60, 85, 75];
+document.getElementById("scores-temp-container").innerHTML = scoresTemp.join(", ");
