198cc36a3a19593652d2f9cd0770bc198f2c1e64
Victor Hugo Segundo Aguilar <victorhugo.webdeveloper@outlook.com>
2025-03-04 18:15:56 -0600

    Adding data structure linear table

--- linear-table/main.js
+++ linear-table/main.js
@@ -1,19 +1,43 @@
-// let scores = new Array(90,70,50,80,60,85);
+function append(array, value) {
+  // Crear una nueva matriz, length = scores.length +
+  let tempArray = new Array(array.length + 1)
 
-// for(let i = 0; i < scores.length; i++) {
-//   document.write(scores[i] + ",");
+  for(let i = 0; i < array.length; i++) { // Copiando el valor de la matriz a tempArray
+    tempArray[i] = array[i]
+  }
+
+  tempArray[array.length] = value // Añadiendo el nuevo valor al final de tempArray
+  return tempArray // Devolviendo la nueva matriz
+}
+
+// Test
+let scores = new Array(90,70,50,80, 60, 85)
+
+scores = append(scores, 75)
+
+for(var i = 0; i < scores.length; i++) {
+  document.write(scores[i] + ",")
+}
+
+// function append(array, value) {
+//   // Crear una nueva matriz con un espacio adicional
+//   let tempArray = new Array(array.length + 1);
+
+//   // Copiando los valores existentes a tempArray
+//   for (let i = 0; i < array.length; i++) {
+//     tempArray[i] = array[i];
+//   }
+
+//   // Añadiendo el nuevo valor al final de tempArray
+//   tempArray[array.length] = value; // ← Aquí estaba el error
+
+//   return tempArray; // Devolviendo la nueva matriz
 // }
 
-// let scoresTemp = new Array(90,70,50,80,60,85, 75);
+// // Test
+// let scores = [90, 70, 50, 80, 60, 85];
 
-// for(let i = 0; i < scoresTemp.length; i++) {
-//   document.write(scoresTemp[i] + ",");
-// }
+// scores = append(scores, 75);
 
-// Primer arreglo
-let scores = [90, 70, 50, 80, 60, 85];
-document.getElementById("scores-container").innerHTML = scores.join(", ");
-
-// Segundo arreglo
-let scoresTemp = [90, 70, 50, 80, 60, 85, 75];
-document.getElementById("scores-temp-container").innerHTML = scoresTemp.join(", ");
+// // Imprimir los valores en el DOM en un div
+// document.getElementById("scores-container").innerHTML = scores.join(", ");
